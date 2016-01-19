fb10148b0997ece22266dff4a0afbe3f9cbb4973
Sean Rivis <sean.rivis@gmail.com>
2016-01-19 17:36:51 -0500

    add home button, add jquery to make main class responsive to nav clicks, add html files

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,3 +7,34 @@
   });
 
 })(jQuery, window, document);
+
+
+
+// $(document.ready(function(){
+
+// });
+
+var spoon = $('.main');
+var poon = spoon.html();
+console.log(poon);
+$(document).ready(function(){
+
+                 $('#home').click(function(){
+                     $('.main').html(poon);
+                 });
+});
+$(document).ready(function(){
+                 $('#library').click(function(){
+                     $('.main').load('library.html');
+                 });
+});
+$(document).ready(function(){
+                 $('#checkedout').click(function(){
+                     $('.main').load('checkedout.html');
+                 });
+});
+$(document).ready(function(){
+                 $('#faq').click(function(){
+                 $('.main').load('faq.html');
+                 });
+});
