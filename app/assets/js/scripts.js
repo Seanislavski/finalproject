9744e1fd77d7d7f7e6e64b0d2fba9189037533c7
Sean <Sean.Rivis@gmail.com>
2016-01-23 19:52:38 -0500

    Changed search result book layout.

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -32,16 +32,64 @@ $(document).ready(function(){
 });
 $(document).ready(function(){
                  $('#library').click(function(){
+                    defaultContent = mainArea.html();
                      $('.main').load('library.html');
                  });
 });
 $(document).ready(function(){
+                 defaultContent = mainArea.html();
                  $('#checkedout').click(function(){
                      $('.main').load('checkedout.html');
                  });
 });
 $(document).ready(function(){
+                defaultContent = mainArea.html();
                  $('#faq').click(function(){
                  $('.main').load('faq.html');
                  });
 });
+
+function handleResponse(response) {
+    for (var i = 0; i < response.items.length; i++) {
+        var item = response.items[i];
+        var results = item.volumeInfo; 
+        // in production code, item.text should have the HTML entities escaped.
+        // var main = document.getElementsByClassName('searchResults')[0]
+        var main = $('.searchResults')[0];
+        //main.innerHTML += '<br>' + results.title + ' by ' + results.authors;
+        var bookStop = "<div class='books' id='book" + i + "'></div>";
+        $('#searchResults').append(bookStop);
+
+            //^ append makes sure that multiple searches means more divs with the same ID
+            // if use html() instead it only allows one 'book'
+
+        $('#book' + [i]).html('<h3>' + results.title + '</h3><br>by ' + results.authors + '<p>Description: ' + results.description + '</p>');
+       
+   } //end for statement
+}  //query API and put in #main
+
+//get form to work
+var newBook = $('#newBook'); //button
+
+var searchForm = $('search'); //form
+
+newBook.click(function(event){
+    event.preventDefault();
+    var queryId = searchInput.value; //text field    
+    
+    // console.log(typeof queryId);
+    var results = ('"https://www.googleapis.com/books/v1/volumes?q=' + queryId + '&callback=handleResponse"');
+    var scrInject = ('<script src=' + results + '></script>');
+
+    var putScript = $('#putScript');
+
+    $('#putScript').html(scrInject);
+
+    console.log(queryId);
+    // console.log(scrInject);
+
+}); //button press
+
+// function searchHard(){
+//     console.log("ok");
+// }
