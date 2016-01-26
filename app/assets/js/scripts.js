00407efd9375d9e980d93ad3a87bee24bd0d2bbe
Sean Rivis <sean.rivis@gmail.com>
2016-01-26 17:34:33 -0500

    change books to images / covers

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -53,15 +53,28 @@ var numBooks;
 var books = $('.books');
 function handleResponse(response) {
     $('#searchResults').html(''); //clear area
+
+    // for(var x = 0; x < response.items.length; x++) {
+    //     var bookStop = "<div class='books' id='book" + x + "'></div>";
+    //     $('#searchResults').append(bookStop);
+    // };
+    // window.numBooks = response.items.length;
+
     for (var i = 0; i < response.items.length; i++) {
         var item = response.items[i];
         var results = item.volumeInfo;
         // var main = $('.searchResults')[0];
         var bookStop = "<div class='books' id='book" + i + "'></div>";
         $('#searchResults').append(bookStop);
-        $('#book' + [i]).html('<h3>' + results.title + '</h3><br>by ' + results.authors + '<span class="bookContent"><p>Description: ' + results.description + '</p><img src="' + results.imageLinks.thumbnail + '"></img>&nbsp;</span><span class="addToLibrary"><button id="btn' + i + '">Add To Library</button></span>');
+        console.log(results.imageLinks.medium);
+        if (results.imageLinks.medium === true){
+
+            $('#book' + [i]).html('<img src="' + results.imageLinks.medium + '"></img>');
+        } else{
+        $('#book' + [i]).html('<img src="' + results.imageLinks.thumbnail + '"></img>');
+        }
+        // $('#book' + [i]).html('<h3>' + results.title + '</h3><br>by ' + results.authors + '<span class="bookContent"><p>Description: ' + results.description + '</p><img src="' + results.imageLinks.thumbnail + '"></img>&nbsp;</span><span class="addToLibrary"><button id="btn' + i + '">Add To Library</button></span>');
    } //end for statement
-   numBooks = books.length;
 }  //query API and put in #main
 
 //
@@ -71,32 +84,20 @@ var newBook = $('#newBook'); //Search button
 
 newBook.click(function(event){
     event.preventDefault();
-
     // var searchInput = $('#searchInput');
     var queryId = searchInput.value; //text field
-
-
     // console.log(typeof queryId);
     var results = ('"https://www.googleapis.com/books/v1/volumes?q=' + queryId + '&callback=handleResponse"');
     var scrInject = ('<script src=' + results + '></script>');
-
-    // var putScript = $('#putScript');
-
     $('#putScript').html(scrInject);
-
     // console.log(queryId);
     // console.log(scrInject);
-
 }); //button press
-
-
-
 
 // var addToLibrary = $('.addToLibrary');
 // var addToButton = $('Hello');
 
-var addIfClicked = ('#btn' + numBooks);
-addIfClicked.click(function(event){
-    event.preventDefault();
-
-});
+// var addIfClicked = ('#btn' + numBooks);
+// addIfClicked.click(function(event){
+//     event.preventDefault();
+// });
