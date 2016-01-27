76592e356e5f7fde156140532fb02f45bb4f27e3
Sean Rivis <sean.rivis@gmail.com>
2016-01-27 17:31:57 -0500

    added maxResults, changed sizes

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -56,7 +56,7 @@ function handleResponse(response) {
         var item = response.items[i];
         var results = item.volumeInfo;
         var next = (i + 1);
-        var bookStop = "<div class='flipper'><div class='books front' id='book" + i + "'></div><div class='books back' id='bookB" + i + "'></div></div>";
+        var bookStop = "<div class='area'><div class='books' id='book" + i + "'></div></div>";
         $('#searchResults').append(bookStop);
         console.log(results.imageLinks.medium);
         if (results.imageLinks.medium === true){
@@ -80,7 +80,9 @@ newBook.click(function(event){
     event.preventDefault();
     var queryId = searchInput.value;
     $.getJSON('https://www.googleapis.com/books/v1/volumes', {
-                q: queryId}, handleResponse);
+                q: queryId,
+                maxResults: 40}, handleResponse);
+
 });
 
 
@@ -107,3 +109,40 @@ newBook.click(function(event){
 // addIfClicked.click(function(event){
 //     event.preventDefault();
 // });
+/*
+    Vertigo.js
+    Simple vertical and horizontal parallax with no jQuery or Stellar.js
+    Ed Aponte 11.04.2015
+
+*/
+
+
+var parallaxY = document.getElementsByClassName("parallaxY"); // get an array of all elements with class parallaxY
+var speedY = []; // declare a speedY array that will be used to store data-speeds for each class parallaxY
+
+var parallaxX = document.getElementsByClassName("parallaxX"); // get an array of all elements with class parallaxX
+var speedX = []; // declare a speedX array that will be used to store data-speeds for each class parallaxX
+
+
+window.onscroll = function() { // window.onscroll will perform the below code whenever you scroll
+
+    // Loop thru all classes parallaxY, get their corresponding data-speed value and apply the vertical offset to the background
+    for ( var y = 0 ; y < parallaxY.length ; y++) {
+
+        speedY[y] = parallaxY[y].getAttribute("data-speed");
+        var yOffset = window.pageYOffset;
+        parallaxY[y].style.backgroundPosition = "0px " + (yOffset / speedY[y]) + "px";
+
+    }
+
+    // Loop thru all classes parallaxX, get their corresponding data-speed value and apply the horizontal offset to the background
+    for ( var x = 0 ; x < parallaxX.length ; x++) {
+
+        speedX[x] = parallaxX[x].getAttribute("data-speed");
+        var xOffset = window.pageXOffset;
+        parallaxX[x].style.backgroundPosition = (xOffset / speedX[x]) + "px" + " 0px";
+
+    }
+
+};
+
