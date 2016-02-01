d5777818ed13f5d13afd553b5b89d3a527780f95
Sean <Sean.Rivis@gmail.com>
2016-02-01 09:06:46 -0500

    Successfully added the beginning of book info to the myLibrary page

--- gulpfile.js
+++ gulpfile.js
@@ -47,6 +47,19 @@ gulp.task('js',function(){
     .pipe(browserSync.reload({stream:true, once: true}));
 });
 
+// gulp.task('js',function(){
+//   gulp.src('src/js/myLibrary.js')
+//     .pipe(jshint('.jshintrc'))
+//     .pipe(jshint.reporter('default'))
+//     .pipe(header(banner, { package : package }))
+//     .pipe(gulp.dest('app/assets/js'))
+//     // .pipe(uglify())
+//     .pipe(header(banner, { package : package }))
+//     .pipe(rename({ suffix: '.min' }))
+//     .pipe(gulp.dest('app/assets/js'))
+//     .pipe(browserSync.reload({stream:true, once: true}));
+// });
+
 gulp.task('browser-sync', function() {
     browserSync.init(null, {
         server: {
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -15,37 +15,59 @@
 // });
 
 var mainArea = $('.main');
-var defaultContent = mainArea.html();
+// var defaultContent = mainArea.html(); //only stores HTML not JS so books will not flip after search if "home" clicked on
 
 $(document).ready(function(){
                  $('#home').click(function(){
-                     $('.main').html(defaultContent);
+                     // $('.main').html(defaultContent);
+                    // defaultContent = mainArea.html();
+                     $('.main').load('main.html'); 
                  });
 });
 $(document).ready(function(){
                  $('#library').click(function(){
-                    defaultContent = mainArea.html();
+                    // defaultContent = mainArea.html();
                      $('.main').load('library.html');
                  });
 });
 $(document).ready(function(){
-                 defaultContent = mainArea.html();
+                 // defaultContent = mainArea.html();
                  $('#checkedout').click(function(){
                      $('.main').load('checkedout.html');
                  });
 });
 $(document).ready(function(){
-                defaultContent = mainArea.html();
+                // defaultContent = mainArea.html();
                  $('#faq').click(function(){
                  $('.main').load('faq.html');
                  });
 });
 
-var addToLibrary = function(){
-    // $('main.library.html')append($(this).parent());
-    // var window.bookToAdd = $(this).parent();
+var libraryArray = []; //array to store info for search on myLibrary
+
+var readList = $('#listOfBooks');
+
+function notMe(){
+    // .remove();    
 }
-var flipIt = function(){
+
+function addToList(){
+    var bookToList = $(this).siblings('.title2');
+    var isbnToList = $(this).siblings('.hiddenIsbn');
+    // var myLib = $('#myLibrary');
+    
+    // if()
+    readList.append('<li><a onclick="notMe()" class="notThis">x</a> &nbsp;<span class="listedBook">' + bookToList.html() + '</span><br>'); //add to List
+    libraryArray.push(isbnToList.html());
+    var evOth = $('#listOfBooks:nth-child(odd)'); //every other listed book
+    evOth.css('background', '#444444'); //now has a grey bg
+}
+
+var x = $('.notThis');
+// x.
+
+
+function flipIt(){
     $('.flip').click(function(){
         $(this).find('.card').addClass('flipped').mouseleave(function(){
             $(this).removeClass('flipped');
@@ -61,45 +83,53 @@ var books = $('.books');
 function handleResponse(response) {
     $('#searchResults').html(''); //clear area
 
-    // for(var x = 0; x < response.items.length; x++) {
-    //     var bookStop = "<div class='books' id='book" + x + "'></div>";
-    //     $('#searchResults').append(bookStop);
-    // };
-    // window.numBooks = response.items.length;
-
     for (var i = 0; i < response.items.length; i++) {
         var item = response.items[i];
         var results = item.volumeInfo;
         var next = (i + 1);
         var bookStop = "<div class='flip'><div class='card'><div class='books face front' id='book" + i + "'></div><div class='books back' id='bookB" + i + "'></div></div></div>";
+        if (results.industryIdentifiers){
+            var hiddenIsbn = results.industryIdentifiers[0]['identifier'];
+        } else{
+            var hiddenIsbn;
+        }
         $('#searchResults').append(bookStop);
-        // console.log(results.imageLinks.medium);
-        // if (results.imageLinks.medium === true){
 
-        //     $('#book' + i).html('<img src="' + results.imageLinks.medium + '"></img>');
-        //     $('#bookB' + i).html('Hello');
-        // } else{
             if(results.imageLinks){
                 $('#book' + i).html('<img src="' + results.imageLinks.thumbnail + '"></img>');
             }
-        $('#bookB' + i).html("<span class='title2'>" + results.title + '</span><span class="by"><br>By ' + results.authors + "</span><span class='add'>Add</span>");
+        $('#bookB' + i).html("<span class='title2'>" + results.title + '</span><span class="by"><br>By ' + results.authors + "</span><span class='hiddenIsbn'>" + hiddenIsbn + "</span><span class='add'>Add</span>");
+
    } //end for statement
 
 flipIt();//call the flip in the loop!
-var addButton = $('.add');
-addButton.click(addToLibrary);
+var addButton = $('.add'); //Add button on book
+addButton.click(addToList); // call addToList function when clicked
 }  //query API and put in #main
 
 //
 var newBook = $('#newBook'); //Search button
-
+var queryId;
 newBook.click(function(event){
     event.preventDefault();
-    var queryId = searchInput.value;
+    queryId = searchInput.value;
     $.getJSON('https://www.googleapis.com/books/v1/volumes', {
                 q: queryId,
                 maxResults: 40}, handleResponse);
+
 });
+
+function libraryBooks(response){
+    var myLibrary = $('#myLibrary');
+    if (response.items){
+        var item = response.items[0];    
+    } else{
+        var item;
+    }
+    
+    var results = item.volumeInfo;
+    myLibrary.append('<div class="book"><div class="bookCover"><img src="' + results.imageLinks.thumbnail + '"></img></div><div class="contents">' + results.title + '<br> By: ' + results.authors + '<br>' + results.description + '</div></div>' );
+}
 
 
 
@@ -115,7 +145,7 @@ newBook.click(function(event){
     // console.log(queryId);
     // console.log(scrInject);
 }); //button press
-*/
+*///old way of calling API
 
 
 
@@ -126,6 +156,9 @@ newBook.click(function(event){
 // addIfClicked.click(function(event){
 //     event.preventDefault();
 // });
+
+
+
 /*
     Vertigo.js
     Simple vertical and horizontal parallax with no jQuery or Stellar.js
